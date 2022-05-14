c1731bd8902cefe68d9783a25d923de080857c4a
Ernest <chidiebereernest8@gmail.com>
2022-05-14 07:54:30 +0100

    Added the sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
 import Nopage from "./pages/Nopage";
-import Sidebar from "./components/Sidebar";
+import Render from "./components/Render";
+import Logout from "./pages/Logout";
 
 function App() {
   return (
@@ -12,11 +15,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Auth />} />
-          <Route path="/" element={<Sidebar />}>
+          <Route path="/" element={<Render />}>
             <Route index element={<Dashboard />} />
             <Route path="product" element={<Products />} />
+            <Route path="logout" element={<Logout />} />
+            <Route path="*" element={<Nopage />} />
           </Route>
-          <Route path="/*" element={<Nopage />} />
         </Routes>
       </BrowserRouter>
     </>
