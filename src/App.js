43febd5f7bf7925ceaf423532c04b87848aeb683
Ernest <chidiebereernest8@gmail.com>
2022-05-17 02:35:04 +0100

    New commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import Auth from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
+import Products from "./pages/Products";
+import Clients from "./pages/Clients";
+import Teams from "./pages/Teams";
+import Nopage from "./pages/Nopage";
+import Render from "./components/Render";
+import Logout from "./pages/Logout";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Auth />} />
+          <Route path="/" element={<Render />}>
+            <Route index element={<Dashboard />} />
+            <Route path="product" element={<Products />} />
+            <Route path="client" element={<Clients />} />
+            <Route path="team" element={<Teams />} />
+            <Route path="logout" element={<Logout />} />
+            <Route path="*" element={<Nopage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Nopage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NoPage = () => {
+  return (
+    <div>
+      <h2>Not Found Page</h2>
+    </div>
+  );
+};
+
+export default NoPage;
